0afa4494fbe74dad3693f800a3059519beab990b
linabiel <linuka.b@gmail.com>
2021-04-16 19:44:49 +0100

    nothing works

--- controllers/controller.py
+++ controllers/controller.py
@@ -8,10 +8,10 @@ def index():
     return render_template('index.html', title='Home')
 
 
-@app.route('/<player1><player2>', methods=['POST'])
-def play_game(player1, player2):
-    # player1 = request.form['gesture1']
-    # player2 = request.form['gesture2']
+@app.route('/')
+def play(player1, player2):
+    player1 = request.form['gesture1']
+    player2 = request.form['gesture2']
     game = Game()
-    # winner = game.compare_choices(player1, player2)
+    winner = game.compare_choices(player1, player2)
     return render_template('index.html', winner=game.compare_choices(player1, player2))
--- modules/game.py
+++ modules/game.py
@@ -1,10 +1,13 @@
 class Game:
     
     def compare_choices(self, player1, player2):
-        if player1.choice == "Rock" and player2.choice == "Scissors":
-            return player1
-        if player1.choice == "Rock" and player2.choice == "Paper":
-            return player2
-        if player1.choice == "Paper" and player2.choice == "Scissors":
-            return player2
+        if player1.choise == player2.choise:
+            return None
+        else:
+            if player1.choice == "Rock" and player2.choice == "Scissors":
+                return player1
+            if player1.choice == "Rock" and player2.choice == "Paper":
+                return player2
+            if player1.choice == "Paper" and player2.choice == "Scissors":
+                return player2
 
