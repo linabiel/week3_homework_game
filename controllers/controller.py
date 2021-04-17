63981648cad29636a626000989ac7c7173deb965
linabiel <linuka.b@gmail.com>
2021-04-17 17:00:06 +0100

    you vs computer works

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from app import app
-from modules.game import play
+from modules.game import play, generate_computer_gesture
 
 @app.route('/')
 def home():
@@ -12,13 +12,20 @@ def enter_gestures_into_url(gesture1, gesture2):
 
 @app.route('/play')
 def select_gesture_get():
-    return render_template('play.html')
+    return render_template('play_use_select.html')
 
 @app.route('/play', methods=['POST'])
 def select_gesture():
-    gesture1 = request.form["gesture1"]
+    gesture1 = request.form['gesture1']
     gesture2 = request.form['gesture2']
-    print(gesture1)
-    print(gesture2)
-    print(play(gesture1, gesture2))
+    return render_template('url.html', winner=play(gesture1, gesture2))
+
+@app.route('/comp')
+def play_against_computer_get():
+    return render_template('play_with_computer.html')
+
+@app.route('/comp', methods=['POST'])
+def play_against_computer():
+    gesture1 = generate_computer_gesture()
+    gesture2 = request.form['gesture2']
     return render_template('url.html', winner=play(gesture1, gesture2))
--- modules/game.py
+++ modules/game.py
@@ -1,3 +1,5 @@
+import random
+
 def play(gesture1, gesture2):
     gesture1 = gesture1.lower()
     gesture2 = gesture2.lower()
@@ -11,8 +13,14 @@ def play(gesture1, gesture2):
         return "Player 2 wins by playing Paper"
     if gesture1 == "paper" and gesture2 == "rock":
         return "Player 1 wins by playing Paper"
-
     if gesture1 == "paper" and gesture2 == "scissors":
         return "Player 2 wins by playing Scissors"
     if gesture1 == "scissors" and gesture2 == "paper":
         return "Player 1 wins by playing Scissors"
+    return "Invalid input"
+
+def generate_computer_gesture():
+    gestures = ["rock", "paper", "scissors"]
+    computer_gesture = random.choice(gestures)
+    return computer_gesture
+
